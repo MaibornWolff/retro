cbd690f2476b96b077420e108ce4d5d7ff54910f
Yadullah Duman <yadullah.duman@gmail.com>
2018-09-03 15:04:20 +0200

    remove obsolete jsx file

--- frontend/src/components/common/Title.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Title = ({ className, children }) => (
-  <h3 id="column-title" className={`${className} title is-3`}>
-    {children}
-  </h3>
-);
-
-export default Title;
