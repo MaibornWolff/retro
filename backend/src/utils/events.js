03324bda891b6322a822172d0139588ea7510f81
Yadullah Duman <yadullah.duman@gmail.com>
2018-09-09 02:47:51 +0200

    refactored event constants

--- backend/src/utils/events.js
+++ backend/src/utils/events.js
@@ -1,11 +1,6 @@
-const CONNECTION = "connection";
-const DISCONNECT = "disconnect";
-const CREATE_CARD = "create card";
-const CREATE_COLUMN = "create column";
-
 module.exports = {
-  CONNECTION,
-  DISCONNECT,
-  CREATE_CARD,
-  CREATE_COLUMN
-}
+  CONNECTION: "connection",
+  DISCONNECT: "disconnect",
+  CREATE_CARD: "create card",
+  CREATE_COLUMN: "create column"
+};
