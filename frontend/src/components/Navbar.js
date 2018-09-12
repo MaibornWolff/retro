9b374dac211241d8467b20130274ff31758187a7
Yadullah Duman <yadullah.duman@gmail.com>
2018-09-12 17:22:03 +0200

    replaced alert with modal for now on settings

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Modal from "react-responsive-modal";
 import { Link } from "@reach/router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCog } from "@fortawesome/free-solid-svg-icons";
@@ -6,34 +7,55 @@ import { faCog } from "@fortawesome/free-solid-svg-icons";
 import Button from "./common/Button";
 
 import "../styles/Navbar.css";
+import "../styles/Modal.css";
 
-const handleClick = () => alert("you clicked the settings button!");
+export default class Navbar extends React.Component {
+  state = { open: false };
 
-const Navbar = () => (
-  <header id="app-header">
-    <nav className="navbar is-info" aria-label="main navigation">
-      <div className="navbar-brand">
-        <div className="navbar-item">
-          <Link id="navbrand" to="/">
-            Retro
-          </Link>
-        </div>
-      </div>
+  onOpenModal = () => this.setState({ open: true });
 
-      <div className="navbar-end">
-        <div className="navbar-item">
-          <div className="field is-grouped">
-            <p className="control">
-              <Button className="is-info is-rounded is-inverted is-outlined" onClick={handleClick}>
-                <FontAwesomeIcon icon={faCog} />
-                &nbsp;Settings
-              </Button>
-            </p>
+  onCloseModal = () => this.setState({ open: false });
+
+  render() {
+    const { open } = this.state;
+
+    return (
+      <header id="app-header">
+        <nav className="navbar is-info" aria-label="main navigation">
+          <div className="navbar-brand">
+            <div className="navbar-item">
+              <Link id="navbrand" to="/">
+                Retro
+              </Link>
+            </div>
           </div>
-        </div>
-      </div>
-    </nav>
-  </header>
-);
 
-export default Navbar;
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="field is-grouped">
+                <p className="control">
+                  <Button
+                    className="is-info is-rounded is-inverted is-outlined"
+                    onClick={this.onOpenModal}
+                  >
+                    <FontAwesomeIcon icon={faCog} />
+                    &nbsp;Settings
+                  </Button>
+                </p>
+
+                <Modal
+                  open={open}
+                  onClose={this.onCloseModal}
+                  center
+                  classNames={{ modal: "custom-modal" }}
+                >
+                  <p>Settings Here</p>
+                </Modal>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+}
