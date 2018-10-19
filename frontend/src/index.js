088546bb95f23823021197cb928199e3af9442d1
Yadullah Duman <yadullah.duman@gmail.com>
2018-10-19 13:38:58 +0200

    removed obsolete line

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,6 @@ import Retro from "./components/Retro";
 import registerServiceWorker from "./registerServiceWorker";
 
 import "../node_modules/bulma/css/bulma.min.css";
-import "../node_modules/bulma-switch/dist/css/bulma-switch.min.css";
 
 ReactDOM.render(<Retro />, document.getElementById("root"));
 registerServiceWorker();
