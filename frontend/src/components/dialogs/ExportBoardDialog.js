5cd18606a58f8562abbb363b0e25cf3d2bec8cc3
Yadullah Duman <yadullah.duman@gmail.com>
2019-03-13 15:27:13 +0100

    refactored export board button and added tests for it

---
+++ frontend/src/components/dialogs/ExportBoardDialog.js
@@ -0,0 +1,68 @@
+import React from "react";
+import ExportIcon from "@material-ui/icons/PictureAsPdf";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button,
+  withMobileDialog
+} from "@material-ui/core";
+
+const endpoint = "/api/boards/export/";
+const port = "8081";
+const exportURL = `http://${window.location.hostname}:${port}${endpoint}`;
+
+class ExportBoardDialog extends React.Component {
+  state = { open: false };
+
+  openDialog = () => this.setState({ open: true });
+
+  closeDialog = () => this.setState({ open: false });
+
+  render() {
+    const { open } = this.state;
+    const { boardId, fullScreen } = this.props;
+
+    return (
+      <>
+        <Button
+          size="small"
+          variant="outlined"
+          aria-label="Export Board"
+          color="primary"
+          onClick={this.openDialog}
+          data-testid="export-board-btn"
+        >
+          <ExportIcon style={{ marginRight: 5 }} />
+          Export Board
+        </Button>
+        <Dialog
+          fullScreen={fullScreen}
+          open={open}
+          onClose={this.closeDialog}
+          aria-labelledby="board-export-dialog"
+          aria-describedby="board-export-dialog-description"
+        >
+          <DialogTitle id="board-export-dialog">Export Board</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="board-export-dialog-description">
+              Do you want to export this board?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button color="primary" onClick={this.closeDialog}>
+              Cancel
+            </Button>
+            <Button color="primary" href={exportURL + boardId}>
+              Export
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </>
+    );
+  }
+}
+
+export default withMobileDialog()(ExportBoardDialog);
