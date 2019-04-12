c49d37bce03c28e320e9bdd5b756332af76155d6
Yadullah Duman <yadullah.duman@gmail.com>
2019-04-12 11:31:59 +0200

    made RetroItem component a PureComponent

--- frontend/src/components/RetroItem.js
+++ frontend/src/components/RetroItem.js
@@ -15,51 +15,61 @@ import DeleteItemDialog from "./dialogs/DeleteItemDialog";
 import UpvoteItemButton from "./buttons/UpvoteItemButton";
 import { CardWrapper, CardContainer, CardText, CardAuthor } from "./styled";
 
-const RetroItem = props => {
-  const { classes, id, author, content, points, boardId, isBlurred } = props;
+class RetroItem extends React.PureComponent {
+  render() {
+    const {
+      classes,
+      id,
+      author,
+      content,
+      points,
+      boardId,
+      isBlurred
+    } = this.props;
 
-  return (
-    <CardWrapper isBlurred={isBlurred}>
-      <CardContainer>
-        <Card className={classes.card} raised>
-          <CardHeader
-            avatar={
-              <Avatar className={classes.avatar} aria-label="number of votes">
-                {points}
-              </Avatar>
-            }
-            title={
-              <Typography variant="body2" component={"span"}>
-                <CardAuthor>{author}</CardAuthor>
-              </Typography>
-            }
-          />
-          <Divider />
-          <CardContent>
-            <Typography
-              variant="body2"
-              className={classes.contentBody}
-              component={"span"}
-            >
-              <CardText>{content}</CardText>
-            </Typography>
-          </CardContent>
-          <Divider />
-          <CardActions className={classes.actions}>
-            <DeleteItemDialog id={id} boardId={boardId} />
-            <EditItemDialog
-              id={id}
-              author={author}
-              content={content}
-              boardId={boardId}
+    return (
+      <CardWrapper isBlurred={isBlurred}>
+        <CardContainer>
+          <Card className={classes.card} raised>
+            <CardHeader
+              avatar={
+                <Avatar className={classes.avatar} aria-label="number of votes">
+                  {points}
+                </Avatar>
+              }
+              title={
+                <Typography variant="body2" component={"span"}>
+                  <CardAuthor>{author}</CardAuthor>
+                </Typography>
+              }
             />
-            <UpvoteItemButton id={id} boardId={boardId} points={points} />
-          </CardActions>
-        </Card>
-      </CardContainer>
-    </CardWrapper>
-  );
-};
+            <Divider />
+            <CardContent>
+              <Typography
+                variant="body2"
+                className={classes.contentBody}
+                component={"span"}
+              >
+                <CardText>{content}</CardText>
+              </Typography>
+            </CardContent>
+            <Divider />
+            <CardActions className={classes.actions}>
+              <DeleteItemDialog id={id} boardId={boardId} />
+              <EditItemDialog
+                id={id}
+                author={author}
+                content={content}
+                boardId={boardId}
+              />
+              <UpvoteItemButton id={id} boardId={boardId} points={points} />
+            </CardActions>
+          </Card>
+        </CardContainer>
+      </CardWrapper>
+    );
+  }
+}
 
 const styles = {
   avatar: {
