63475d290148d651a7a123cbf553ac61fe725af3
Yadullah Duman <yadullah.duman@gmail.com>
2019-06-08 15:05:25 +0200

    extending BoardContext did not work as I wanted

--- frontend/src/components/context/BoardContext.js
+++ frontend/src/components/context/BoardContext.js
@@ -11,31 +11,3 @@ export const BoardContextProvider = props => {
     </BoardContext.Provider>
   );
 };
-
-/*
-import React from "react";
-
-import { connectSocket } from "../../utils";
-
-export const BoardContext = React.createContext();
-
-let socket;
-
-function sendEvent(eventName, values) {
-  socket.emit(eventName, ...values);
-}
-
-export const BoardContextProvider = props => {
-  const boardId = props.match.params.boardId;
-
-  if (!socket) {
-    socket = connectSocket(boardId);
-  }
-
-  return (
-    <BoardContext.Provider value={{ boardId, sendEvent }}>
-      {props.children}
-    </BoardContext.Provider>
-  );
-};
-*/
