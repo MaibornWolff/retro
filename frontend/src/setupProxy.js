e7a1f84609d33bbd5894911ca479b0ceb6e6a2ee
Yadullah Duman <yadullah.duman@gmail.com>
2020-03-03 21:31:57 +0100

    fix http-proxy-middleware breaking change

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,9 +1,15 @@
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function(app) {
   if (process.env.DEV_ENV === "DOCKER") {
     const dockerHostIp = process.env.DOCKER_HOST_IP || "host.docker.internal";
-    app.use("/api/*", proxy({ target: `http://${dockerHostIp}:3001`, changeOrigin: true }));
+    app.use(
+      "/api/*",
+      createProxyMiddleware({ target: `http://${dockerHostIp}:3001`, changeOrigin: true })
+    );
   } else {
-    app.use("/api/*", proxy({ target: "http://localhost:3001", changeOrigin: true }));
+    app.use(
+      "/api/*",
+      createProxyMiddleware({ target: "http://localhost:3001", changeOrigin: true })
+    );
   }
 };
