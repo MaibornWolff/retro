6c9cc51e4b4cfc38ad43c307009f9dff93441c02
Yadullah Duman <yadullah.duman@maibornwolff.de>
2020-09-07 12:13:47 +0200

    Change icon for blur column button

--- frontend/src/components/board/columns/column-header/BlurColumnMenuItem.tsx
+++ frontend/src/components/board/columns/column-header/BlurColumnMenuItem.tsx
@@ -1,38 +1,39 @@
 import React, { useContext } from "react";
-import EditIcon from "@material-ui/icons/Edit";
+import UnblurIcon from "@material-ui/icons/BlurOff";
 import { MenuItem, ListItemIcon, ListItemText } from "@material-ui/core";
+
 import { BoardContext } from "../../../../context/BoardContext";
 import { UserContext } from "../../../../context/UserContext";
 import { TOGGLE_COLUMN_BLUR } from "../../../../constants/event.constants";
 import { ROLE_MODERATOR } from "../../../../utils/user.utils";
 
 type BlurColumnMenuItemProperties = {
-    columnId: string;
+  columnId: string;
 };
 
 const BlurColumnMenuItem = React.forwardRef(
-    (props: BlurColumnMenuItemProperties, ref: any) => {
-        const { boardId, socket } = useContext(BoardContext);
-        const { userState } = useContext(UserContext);
+  (props: BlurColumnMenuItemProperties, ref: any) => {
+    const { boardId, socket } = useContext(BoardContext);
+    const { userState } = useContext(UserContext);
 
-        function toggleBlur() {
-            socket.emit(TOGGLE_COLUMN_BLUR, boardId, props.columnId);
-        }
-
-        return (
-            <MenuItem
-                button
-                ref={ref}
-                onClick={toggleBlur}
-                disabled={userState.role !== ROLE_MODERATOR}
-            >
-                <ListItemIcon>
-                    <EditIcon fontSize="small" />
-                </ListItemIcon>
-                <ListItemText primary="Toggle Blur" />
-            </MenuItem>
-        );
+    function toggleBlur() {
+      socket.emit(TOGGLE_COLUMN_BLUR, boardId, props.columnId);
     }
+
+    return (
+      <MenuItem
+        button
+        ref={ref}
+        onClick={toggleBlur}
+        disabled={userState.role !== ROLE_MODERATOR}
+      >
+        <ListItemIcon>
+          <UnblurIcon fontSize="small" />
+        </ListItemIcon>
+        <ListItemText primary="Toggle Blur" />
+      </MenuItem>
+    );
+  }
 );
 
 export default BlurColumnMenuItem;
