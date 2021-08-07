ebaef0b15494d84abd23d63623d308e2caf6e496
Yadullah Duman <yadullah.duman@maibornwolff.de>
2021-08-07 19:30:43 +0200

    fix(retro): adjust width of skeleton

--- frontend/src/components/board/items/BlurredItem.tsx
+++ frontend/src/components/board/items/BlurredItem.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { Skeleton } from "@material-ui/lab";
 
 export default function BlurredItem() {
-  return <Skeleton variant="rect" width={360} height={180} animation={false} />;
+  return <Skeleton variant="rect" width={370} height={180} animation={false} />;
 }
