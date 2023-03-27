5ed32659b149bacdfa6a814b3eb3c05f7dd91b21
Lukas Richter <lukas.richter@maibornwolff.de>
2023-03-27 16:06:27 +0200

    feat: inline styled components in file

--- packages/frontend/src/retro/components/columns/Column.tsx
+++ packages/frontend/src/retro/components/columns/Column.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styled, { css } from "styled-components";
 import { Draggable, Droppable } from "react-beautiful-dnd";
 import { Card, Divider } from "@mui/material";
 
@@ -8,26 +7,6 @@ import { ColumnHeader } from "./column-header/ColumnHeader";
 import { RetroColumn } from "../../types/retroTypes";
 import { useUserContext } from "../../../common/context/UserContext";
 import { isModerator } from "../../../common/utils/participantsUtils";
-import { media } from "../../../common/styled-components/styled.utils";
-
-const ColumnContainer = styled(Card)`
-  ${css`
-    display: flex;
-    flex-direction: column;
-    min-width: 25rem;
-    margin: 0.8em 0.2em 0.8em 0.2em;
-    ${media.tablet ? `margin: 0.8em;` : ""}
-  `}
-`;
-
-const CardsContainer = styled.div`
-  ${css`
-    flex: 1;
-    padding: 0.2em;
-    ${media.tablet ? `padding: 1em;` : ""}
-    transition: background-color 0.2s ease;
-  `};
-`;
 
 interface ColumnProps {
   column: RetroColumn;
@@ -39,27 +18,38 @@ export function Column({ column }: ColumnProps) {
   return (
     <Draggable draggableId={column.id} index={column.index} isDragDisabled={!isModerator(user)}>
       {(providedDraggable) => (
-        <ColumnContainer
+        <Card
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            minWidth: "25rem",
+            margin: "0.8em",
+            borderRadius: "15px",
+          }}
           {...providedDraggable.draggableProps}
           {...providedDraggable.dragHandleProps}
           ref={providedDraggable.innerRef}
           elevation={3}
-          style={{ borderRadius: "15px" }}
         >
           <ColumnHeader column={column} />
           <Divider />
           <Droppable droppableId={column.id} type="item" isCombineEnabled={true}>
             {(providedDroppable) => (
-              <CardsContainer
+              <div
+                style={{
+                  flex: 1,
+                  padding: "1em",
+                  transition: "background-color 0.2s ease",
+                }}
                 ref={providedDroppable.innerRef}
                 {...providedDroppable.droppableProps}
               >
                 <Cards column={column} />
                 {providedDroppable.placeholder}
-              </CardsContainer>
+              </div>
             )}
           </Droppable>
-        </ColumnContainer>
+        </Card>
       )}
     </Draggable>
   );
