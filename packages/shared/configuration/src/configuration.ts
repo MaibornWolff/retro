b34f3ee0d50d008dbcafb0ac03b48d75b6f00375
Ben Willenbring <ben.willenbring@maibornwolff.de>
2023-05-05 18:46:36 +0200

    fix: change prefix to match nextjs public env pattern

--- packages/shared/configuration/src/configuration.ts
+++ packages/shared/configuration/src/configuration.ts
@@ -3,23 +3,23 @@ import { RetroAppUrl } from "./RetroAppUrl";
 
 function getConfiguration(): ApplicationConfiguration {
   const backendUrl = new RetroAppUrl({
-    protocol: process.env.REACT_APP_BACKEND_PROTOCOL ?? "http",
-    host: process.env.REACT_APP_BACKEND_HOST ?? "localhost",
-    port: process.env.REACT_APP_BACKEND_PORT ?? "3001",
+    protocol: process.env.NEXT_PUBLICBACKEND_PROTOCOL ?? "http",
+    host: process.env.NEXT_PUBLICBACKEND_HOST ?? "localhost",
+    port: process.env.NEXT_PUBLICBACKEND_PORT ?? "3001",
   });
 
   const signalingServerUrl = new RetroAppUrl({
-    protocol: process.env.REACT_APP_SIGNALING_SERVER_PROTOCOL ?? "http",
-    host: process.env.REACT_APP_SIGNALING_SERVER_HOST ?? "localhost",
-    port: process.env.REACT_APP_SIGNALING_SERVER_PORT ?? "3002",
+    protocol: process.env.NEXT_PUBLICSIGNALING_SERVER_PROTOCOL ?? "http",
+    host: process.env.NEXT_PUBLICSIGNALING_SERVER_HOST ?? "localhost",
+    port: process.env.NEXT_PUBLICSIGNALING_SERVER_PORT ?? "3002",
   });
 
   return {
-    logLevel: process.env.REACT_APP_LOG_LEVEL ?? "info",
+    logLevel: process.env.NEXT_PUBLICLOG_LEVEL ?? "info",
     backendUrl,
     signalingServerUrl,
     retro: {
-      maxVoteCount: Number(process.env.REACT_APP_RETRO_MAX_VOTE_COUNT) ?? 3,
+      maxVoteCount: Number(process.env.NEXT_PUBLICRETRO_MAX_VOTE_COUNT) ?? 3,
     },
   };
 }
