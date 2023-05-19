b94ddd3979afd38cc3668544dd20432930cd7ed1
Ben Willenbring <ben.willenbring@maibornwolff.de>
2023-05-19 15:52:05 +0200

    fix: wrong env variable names

--- packages/shared/configuration/src/configuration.ts
+++ packages/shared/configuration/src/configuration.ts
@@ -3,23 +3,23 @@ import { RetroAppUrl } from "./RetroAppUrl";
 
 function getConfiguration(): ApplicationConfiguration {
   const backendUrl = new RetroAppUrl({
-    protocol: process.env.NEXT_PUBLICBACKEND_PROTOCOL ?? "http",
-    host: process.env.NEXT_PUBLICBACKEND_HOST ?? "localhost",
-    port: process.env.NEXT_PUBLICBACKEND_PORT ?? "3001",
+    protocol: process.env.NEXT_PUBLIC_BACKEND_PROTOCOL ?? "http",
+    host: process.env.NEXT_PUBLIC_BACKEND_HOST ?? "localhost",
+    port: process.env.NEXT_PUBLIC_BACKEND_PORT ?? "3001",
   });
 
   const signalingServerUrl = new RetroAppUrl({
-    protocol: process.env.NEXT_PUBLICSIGNALING_SERVER_PROTOCOL ?? "http",
-    host: process.env.NEXT_PUBLICSIGNALING_SERVER_HOST ?? "localhost",
-    port: process.env.NEXT_PUBLICSIGNALING_SERVER_PORT ?? "3002",
+    protocol: process.env.NEXT_PUBLIC_SIGNALING_SERVER_PROTOCOL ?? "http",
+    host: process.env.NEXT_PUBLIC_SIGNALING_SERVER_HOST ?? "localhost",
+    port: process.env.NEXT_PUBLIC_SIGNALING_SERVER_PORT ?? "3002",
   });
 
   return {
-    logLevel: process.env.NEXT_PUBLICLOG_LEVEL ?? "info",
+    logLevel: process.env.NEXT_PUBLIC_LOG_LEVEL ?? "info",
     backendUrl,
     signalingServerUrl,
     retro: {
-      maxVoteCount: Number(process.env.NEXT_PUBLICRETRO_MAX_VOTE_COUNT) ?? 3,
+      maxVoteCount: Number(process.env.NEXT_PUBLIC_RETRO_MAX_VOTE_COUNT) ?? 3,
     },
   };
 }
