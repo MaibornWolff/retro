0a964895554fd9a89e20a4d38c75b1e3ab77b401
Ben Willenbring <ben.willenbring@maibornwolff.de>
2023-09-07 13:59:39 +0200

    test: add debug log to check environment variables during deployment

--- packages/frontend/src/common/utils/globalGetServerSideProps.ts
+++ packages/frontend/src/common/utils/globalGetServerSideProps.ts
@@ -9,5 +9,6 @@ export const globalGetServerSideProps: GetServerSideProps<{
   configuration: ApplicationConfiguration;
 }> = async () => {
   const parsedConfiguration = JSON.parse(JSON.stringify(configuration));
+  console.debug("Configuration", { configuration, env: process.env });
   return { props: { configuration: parsedConfiguration } };
 };
