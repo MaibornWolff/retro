42b22d0e44fe7437f968e7d2ce0eafe25ba718cf
Ben Willenbring <ben.willenbring@maibornwolff.de>
2023-09-07 17:29:49 +0200

    chore: remove configuration log

--- packages/frontend/src/common/utils/globalGetServerSideProps.ts
+++ packages/frontend/src/common/utils/globalGetServerSideProps.ts
@@ -9,6 +9,5 @@ export const globalGetServerSideProps: GetServerSideProps<{
   configuration: ApplicationConfiguration;
 }> = async () => {
   const parsedConfiguration = JSON.parse(JSON.stringify(configuration));
-  console.debug("Configuration", { configuration, env: process.env });
   return { props: { configuration: parsedConfiguration } };
 };
