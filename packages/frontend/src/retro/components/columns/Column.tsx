f0e1a00ac12564c09976b6698adf862ff5b3207c
Ben Willenbring <ben.willenbring@maibornwolff.de>
2023-11-08 18:51:09 +0100

    feat(retro): make columns scrollable instead of page

--- packages/frontend/src/retro/components/columns/Column.tsx
+++ packages/frontend/src/retro/components/columns/Column.tsx
@@ -35,20 +35,29 @@ export function Column({ column }: ColumnProps) {
         >
           <ColumnHeader column={column} />
           <Divider />
-          <Droppable droppableId={column.id} type="item" isCombineEnabled={true}>
-            {(providedDroppable) => (
-              <Box
-                flex={1}
-                p={2}
-                sx={{ transition: "background-color 0.2s ease" }}
-                ref={providedDroppable.innerRef}
-                {...providedDroppable.droppableProps}
-              >
-                <Cards column={column} />
-                {providedDroppable.placeholder}
-              </Box>
-            )}
-          </Droppable>
+          <Box
+            sx={{
+              overflowY: "auto",
+            }}
+          >
+            <Droppable droppableId={column.id} type="item" isCombineEnabled={true}>
+              {(providedDroppable) => (
+                <Box
+                  flex={1}
+                  p={2}
+                  minHeight="100%"
+                  sx={{
+                    transition: "background-color 0.2s ease",
+                  }}
+                  ref={providedDroppable.innerRef}
+                  {...providedDroppable.droppableProps}
+                >
+                  <Cards column={column} />
+                  {providedDroppable.placeholder}
+                </Box>
+              )}
+            </Droppable>
+          </Box>
         </Card>
       )}
     </Draggable>
