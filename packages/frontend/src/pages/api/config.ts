037e806cde010c9e8956188842bb61afb898d500
Manuel Lehé <manuellehe@googlemail.com>
2023-12-21 15:03:05 +0100

    fix: fix undetected configuration use

--- packages/frontend/src/pages/api/config.ts
+++ packages/frontend/src/pages/api/config.ts
@@ -1,9 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { ApplicationConfiguration, configuration } from "@shared/configuration";
+import { ApplicationConfiguration, getConfiguration } from "@shared/configuration";
 
 export default function handler(
   request: NextApiRequest,
   response: NextApiResponse<ApplicationConfiguration>
 ) {
-  response.status(200).json(configuration);
+  response.status(200).json(getConfiguration());
 }
