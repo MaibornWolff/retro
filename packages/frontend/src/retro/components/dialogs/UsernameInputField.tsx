3331c987d7ea0fea63b044e84b22c3e4dcf229d6
Manuel Lehé <manuellehe@googlemail.com>
2024-01-08 15:12:47 +0100

    feat: add tooltip to checkbox and label

--- packages/frontend/src/retro/components/dialogs/UsernameInputField.tsx
+++ packages/frontend/src/retro/components/dialogs/UsernameInputField.tsx
@@ -1,4 +1,4 @@
-import { Checkbox, DialogContentText, FormControlLabel } from "@mui/material";
+import { Checkbox, DialogContentText, FormControlLabel, Tooltip } from "@mui/material";
 import { TextInput } from "../../../common/components/TextInput";
 import React from "react";
 
@@ -34,10 +34,12 @@ export default function UserNameInputField({
         label="Username"
         autoFocus
       />
-      <FormControlLabel
-        control={<Checkbox checked={isStorageAllowed} onChange={onStorageAllowanceChange} />}
-        label={storagePermissionLabel}
-      />
+      <Tooltip title="If checked the user name is saved in your local storage for future sessions. Your data stays always local and is not transmitted to any external service">
+        <FormControlLabel
+          control={<Checkbox checked={isStorageAllowed} onChange={onStorageAllowanceChange} />}
+          label={storagePermissionLabel}
+        ></FormControlLabel>
+      </Tooltip>
     </>
   );
 }
