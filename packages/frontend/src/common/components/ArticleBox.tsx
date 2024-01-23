cccb97795044b632c93c940fef893da8473a9edc
Manuel Lehé <manuellehe@googlemail.com>
2024-01-23 10:54:00 +0100

    fix: add margin to articleBox

--- packages/frontend/src/common/components/ArticleBox.tsx
+++ packages/frontend/src/common/components/ArticleBox.tsx
@@ -4,7 +4,7 @@ import { Box, useTheme } from "@mui/material";
 export default function ArticleBox({ children }: PropsWithChildren) {
   const theme = useTheme();
   return (
-    <Box sx={{ justifyContent: "center", display: "flex", marginTop: theme.spacing(4) }}>
+    <Box sx={{ justifyContent: "center", display: "flex", margin: theme.spacing(4) }}>
       <Box maxWidth="lg">{children}</Box>
     </Box>
   );
