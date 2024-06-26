9c6cb3cc6b765ad205f27c6805259153cd1da44d
Ben Willenbring <ben.willenbring@maibornwolff.de>
2024-06-26 15:14:54 +0200

    chore: update dependencies
    Replace QR code library

--- packages/frontend/src/retro/components/dialogs/QrCodeDialog.tsx
+++ packages/frontend/src/retro/components/dialogs/QrCodeDialog.tsx
@@ -8,30 +8,24 @@ import {
   DialogContentText,
   DialogTitle,
 } from "@mui/material";
-import React, { useRef } from "react";
-import QRCode from "qrcode";
+import React from "react";
+import QRCode from "react-qr-code";
 
 export function QrCodeDialog({ isOpen, close }: DialogProps) {
   const fullScreen = useFullscreen();
-  const qrCanvas = useRef<HTMLCanvasElement>(null);
-
-  async function onRendered() {
-    await QRCode.toCanvas(qrCanvas.current, window.location.href);
-  }
 
   return (
     <Dialog
       fullScreen={fullScreen}
       open={isOpen}
       onClose={close}
-      ref={onRendered}
       aria-labelledby="qr-code-dialog"
       aria-describedby="qr-code-dialog-description"
     >
       <DialogTitle id="qr-code-dialog">QR Code</DialogTitle>
       <DialogContent>
         <DialogContentText id="retro-export-dialog-description">
-          <canvas ref={qrCanvas} />
+          <QRCode value={window.location.href} />
         </DialogContentText>
       </DialogContent>
       <DialogActions>
